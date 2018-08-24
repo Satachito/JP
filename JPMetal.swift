71e3c5e10afeffbde4bd001e6115ece0b367f071
Satoru Ogura <satoru.ogura@me.com>
2018-08-25 02:44:29 +0900

    JPMetal lazy var

--- JPMetal.swift
+++ JPMetal.swift
@@ -30,31 +30,31 @@ TGAFile {
 		return v
 	}
 	
-	var	IDSize 			: UInt8		{ return UI8 (  0 ) }	// Size of ID info following header
-	var	colorMapType	: UInt8		{ return UI8 (  1 ) }	// Whether this is a paletted image
-	var	imageType		: UInt8		{ return UI8 (  2 ) }	// type of image 0=none, 1=indexed, 2=rgb, 3=grey, +8=rle packed
+	lazy	var	IDSize 			: UInt8			= { return UI8 (  0 ) }()	// Size of ID info following header
+	lazy	var	colorMapType	: UInt8			= { return UI8 (  1 ) }()	// Whether this is a paletted image
+	lazy	var	imageType		: UInt8			= { return UI8 (  2 ) }()	// type of image 0=none, 1=indexed, 2=rgb, 3=grey, +8=rle packed
 
-	var	colorMapStart	: Int16		{ return  I16(  3 ) }	// Offset to color map in palette
-	var	colorMapLength	: Int16		{ return  I16(  5 ) }	// Number of colors in palette
-	var	colorMapBpp		: UInt8		{ return UI8 (  7 ) }	// number of bits per palette entry
+	lazy	var	colorMapStart	: Int16			= { return  I16(  3 ) }()	// Offset to color map in palette
+	lazy	var	colorMapLength	: Int16			= { return  I16(  5 ) }()	// Number of colors in palette
+	lazy	var	colorMapBpp		: UInt8			= { return UI8 (  7 ) }()	// number of bits per palette entry
 
-	var	xOffset			: UInt16	{ return UI16(  8 ) }	// Number of pixels to the right to start of image
-	var	yOffset			: UInt16	{ return UI16( 10 ) }	// Number of pixels down to start of image
-	var	width			: UInt16	{ return UI16( 12 ) }	// Width in pixels
-	var	height			: UInt16	{ return UI16( 14 ) }	// Height in pixels
-	var	bitsPerPixel	: UInt8		{ return UI8 ( 16 ) }	// Bits per pixel 8,16,24,32
-	var	descriptor		: UInt8		{ return UI8 ( 17 ) }	// Descriptor bits (flipping, etc)
+	lazy	var	xOffset			: UInt16		= { return UI16(  8 ) }()	// Number of pixels to the right to start of image
+	lazy	var	yOffset			: UInt16		= { return UI16( 10 ) }()	// Number of pixels down to start of image
+	lazy	var	width			: UInt16		= { return UI16( 12 ) }()	// Width in pixels
+	lazy	var	height			: UInt16		= { return UI16( 14 ) }()	// Height in pixels
+	lazy	var	bitsPerPixel	: UInt8			= { return UI8 ( 16 ) }()	// Bits per pixel 8,16,24,32
+	lazy	var	descriptor		: UInt8			= { return UI8 ( 17 ) }()	// Descriptor bits (flipping, etc)
 
-	func
-	Image() -> [ UInt32 ] {
+	lazy	var	image			: [ UInt32 ]	= {
 		let	wNumPixels = Int( width ) * Int( height )
 		var	v = [ UInt32 ]( repeating: 0, count: wNumPixels )
 		rawData.withUnsafeBytes { ( p: UnsafePointer< UInt8 > ) -> Void in
 			switch bitsPerPixel {
 			case 24:
+				let	wStart = 18 + Int( IDSize )
 				for i in 0 ..< wNumPixels {
-					let	w = i * 3
-					v[ i ] = UInt32( p[ w ] ) << 24 + UInt32( p[ w + 1 ] ) << 16 + UInt32( p[ w + 2 ] ) << 8 + UInt32( 0x0ff )
+					let	w = wStart + i * 3
+					v[ i ] = UInt32( p[ w ] ) + UInt32( p[ w + 1 ] ) << 8 + UInt32( p[ w + 2 ] ) << 16 + UInt32( 0x0ff ) << 24
 				}
 			case 32:
 				for i in 0 ..< wNumPixels {
@@ -65,7 +65,7 @@ TGAFile {
 			}
 		}
 		return v
-	}
+	}()
 
 	init( url: URL ) {
 		rawData = try! Data( contentsOf: url )
